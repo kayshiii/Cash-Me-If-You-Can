746e6c9511d8924ab814eca5382deac309e63b75
kayshiii <kassandra_so@dlsu.edu.ph>
2025-11-11 08:51:50 +0800

    added assets
    + title
    - the rest of the game
    + added made assets

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ public class MainMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
     {
+        Debug.Log("Starting Game...");
         SceneManager.LoadScene("Intro Screen");
     }
 
